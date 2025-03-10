5c5502710495eb9c589cdb9c8858a34a40d8c1d9
SofyKov <113056246+SofyKov@users.noreply.github.com>
2025-03-10 13:28:36 -0400

    2

--- Assets/Scripts/AnimatorController.cs
+++ Assets/Scripts/AnimatorController.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AnimatorController : MonoBehaviour
 {
     private Animator animator;
     private CharacterMovement characterMovement;
+    private Rigidbody rb;
     public void Start()
     {
         animator = GetComponent<Animator>();
         characterMovement = GetComponent<CharacterMovement>();
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Portal"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
     public void LateUpdate()
     {
        UpdateAnimator();
@@ -19,6 +30,6 @@ public class AnimatorController : MonoBehaviour
     // TODO Fill this in with your animator calls
     void UpdateAnimator()
     {
-        
+        // animator.SetFloat("CharacterSpeed", rb.linearVelocity.magnitude);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,7 +16,6 @@ public class GameManager : MonoBehaviour
             DontDestroyOnLoad(gameObject);
         }
     }
-
     
     public void IncrementScore()
     {
